e82ba331ae983c39adc9b80e6b81e509b5de50aa
Iasmym Louhanne <iasmym.corporativo@gmail.com>
2025-07-17 08:49:16 -0300

    Fix cart context usage

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '../public/style.css';
+import Layout from '../components/Layout';
+import { CartProvider } from '../components/cartContext';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <CartProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CartProvider>
+  );
 }
