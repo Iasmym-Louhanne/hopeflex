dd6af31e7c7394a5324079e7d8d3f9df690b1421
Iasmym Louhanne <iasmym.corporativo@gmail.com>
2025-07-17 00:43:20 -0300

    fix cart context provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '../public/style.css';
+import { CartProvider } from '../components/cartContext';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>
+  );
 }
